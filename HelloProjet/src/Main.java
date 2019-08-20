015c1a72b46b886e1f84d10f8b34e5dd69e6328e
fengzi <1397854025@qq.com>
2019-08-20 20:30:29 +0800

    最后一次

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("优良的代码！！");
+        System.out.println("再来一次");
     }
 }
